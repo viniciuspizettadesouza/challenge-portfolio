626724b02f1ad78eea45e6dee3f4f6be53fda28a
Vinicius Souza <viniciuspzt@gmail.com>
2020-12-06 03:50:50 -0300

    separete header component, send lat and long to get weather information and show it in the screen

---
+++ challenges/challenge-onsign-tv/original/onsigntv_frontend/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-row class="justify-content-md-center">
+    <b-col cols="12" sm="4">OnSign TV</b-col>
+    <b-col>Forecast</b-col>
+    <b-col>About</b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
