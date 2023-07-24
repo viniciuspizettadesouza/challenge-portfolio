ae5fee941bb5871fc4ae2f0a0e5291dbd0a4ab41
Vinicius Souza <viniciuspzt@gmail.com>
2023-07-24 04:16:04 +0100

    separate in components, stylize selectionmenu

--- challenges/challenge-ingenious-build-frontend/original/src/App.vue
+++ challenges/challenge-ingenious-build-frontend/original/src/App.vue
@@ -1,126 +1,30 @@
 <template>
   <div class="container">
-    <div class="title">Timetable</div>
+    <h1 class="title">Timetable</h1>
 
-    <div class="menu">
-      <div
-        v-for="(item, index) in menuItems"
-        :key="index"
-        :class="{ 'menu-item': true, selected: index === selectedItem }"
-        @click="selectItem(index)"
-      >
-        {{ item }}
-      </div>
-    </div>
+    <MainMenu />
 
-    <div class="bus-lines">
-      <div class="bus-lines-title">Select Bus Line</div>
-      <div class="bus-lines-container">
-        <div
-          v-for="(stop, index) in groupedStopsByLineKeys"
-          :key="index"
-          class="bus-lines-item"
-          @click="selectBusLine(stop)"
-        >
-          {{ stop }}
-        </div>
-      </div>
-    </div>
+    <BusLinesMenu />
 
-    <div class="selection-container">
-      <div class="selection-area">
-        <div class="bus-lines-container">
-          <div v-if="busLineStops !== null" class="selected-stops">
-            <div
-              v-for="stop in busLineStopsKeys"
-              :key="stop"
-              @click="selectBusStop(stop)"
-            >
-              {{ stop }}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="selection-area">
-        <div class="bus-stops-container" v-if="sortedBusLineStops">
-          <div class="title">Title</div>
-          <div class="subtitle">Time</div>
-          <div
-            v-for="(stop, index) in sortedBusLineStops"
-            :key="index"
-            class="time"
-          >
-            {{ stop.time }}
-          </div>
-        </div>
-      </div>
-    </div>
+    <SelectionMenu />
   </div>
 </template>
 
-<script>
-import { ref, onMounted, computed } from "vue";
+<script setup lang="ts">
+import { onMounted } from "vue";
 import { useStore } from "vuex";
+import MainMenu from "./components/MainMenu.vue";
+import BusLinesMenu from "./components/BusLinesMenu.vue";
+import SelectionMenu from "./components/SelectionMenu.vue";
 
-export default {
-  setup() {
-    const store = useStore();
-    const stops = computed(() => store.getters.getStops);
-    const groupedStopsByLineKeys = computed(
-      () => store.getters.getGroupedStopsByLineKeys
-    );
-    const busLineStopsKeys = computed(() => store.getters.getBusLineStopsKeys);
-    const selectedBusLineStop = computed(
-      () => store.getters.getSelectedBusLineStop
-    );
-    const sortedBusLineStops = computed(
-      () => store.getters.getSortedBusLineStops
-    );
+const store = useStore();
 
-    const menuItems = ref(["Bus Lines", "Stops"]);
-    const selectedItem = ref(0);
-    const fetchedStops = ref([]);
-
-    const selectItem = (index) => {
-      selectedItem.value = index;
-    };
-
-    const selectBusLine = (stop) => {
-      store.dispatch("selectBusLine", stops.value[stop]);
-      console.log(selectedBusLineStop.value, stop);
-      if (selectedBusLineStop.value !== stops.value[stop]) {
-        store.dispatch("clearBusStop");
-      }
-    };
-
-    const selectBusStop = (stop) => {
-      store.dispatch("selectBusStop", stop);
-    };
-
-    onMounted(async () => {
-      await store.dispatch("fetchStops");
-      fetchedStops.value = store.state.stops;
-    });
-
-    return {
-      menuItems,
-      selectedItem,
-      selectItem,
-      fetchedStops,
-      stops,
-      groupedStopsByLineKeys,
-      busLineStopsKeys,
-      selectedBusLineStop,
-      sortedBusLineStops,
-      selectBusLine,
-      selectBusStop,
-    };
-  },
-};
+onMounted(async () => {
+  await store.dispatch("fetchStops");
+});
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap");
 
 .container {
@@ -133,104 +37,6 @@ export default {
     font-weight: 600;
     line-height: 32px;
     margin-bottom: 24px;
-  }
-
-  .menu {
-    display: flex;
-    align-items: center;
-    background-color: white;
-    border-radius: 4px;
-    padding: 0px 24px;
-    margin-bottom: 16px;
-
-    .menu-item {
-      display: flex;
-      align-items: center;
-      padding: 20px 24px;
-      cursor: pointer;
-      border-bottom: 2px solid transparent;
-      transition: border-color 0.2s ease-in-out;
-      color: #9a9da4;
-
-      &:hover {
-        border-color: #1952e1;
-      }
-    }
-
-    .selected {
-      border-color: #1952e1;
-      color: #1a1a1a;
-    }
-  }
-  .bus-lines {
-    border-radius: 4px;
-    background: #fff;
-    margin-bottom: 16px;
-    .bus-lines-title {
-      display: flex;
-      padding: 24px 24px 8px 24px;
-      color: #1a1a1a;
-      font-size: 14px;
-      font-weight: 600;
-      line-height: 24px;
-    }
-
-    .bus-lines-container {
-      display: flex;
-      flex-wrap: wrap;
-      padding: 24px;
-      gap: 8px;
-
-      .bus-lines-item {
-        border-radius: 4px;
-        padding: 8px 16px;
-        background: #1952e1;
-        color: #fff;
-        font-size: 12px;
-        font-weight: 500;
-        line-height: 16px;
-      }
-    }
-    .bus-stops-container {
-      .title {
-        color: #1a1a1a;
-        font-size: 14px;
-        font-weight: 600;
-        line-height: 24px;
-      }
-      .subtitle {
-        color: #33373c;
-        font-size: 12px;
-        font-weight: 600;
-        line-height: 16px;
-      }
-      .time {
-        color: #33373c;
-        font-size: 12px;
-        font-weight: 400;
-        line-height: 16px;
-      }
-    }
-  }
-
-  .selection-container {
-    display: flex;
-    gap: 8px;
-    justify-content: space-between;
-
-    .selection-area {
-      width: 50%;
-      border-radius: 4px;
-      border: 2px dashed #9a9da4;
-      background: #fff;
-      color: #63666e;
-      text-align: center;
-      font-size: 14px;
-      font-style: normal;
-      font-weight: 400;
-      line-height: 24px;
-      min-height: 100px;
-    }
   }
 }
 </style>
---
+++ challenges/challenge-ingenious-build-frontend/original/src/components/SelectionMenu.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="selection-container">
+    <span class="selection-area-empty" v-if="!busLineStopsKeys">
+      Please select the bus line first
+    </span>
+    <div v-else class="selection-area">
+      <div class="bus-lines-container">
+        <div class="title">Bus Line: 104</div>
+        <div class="subtitle">Bus Stops</div>
+        <div
+          v-for="stop in busLineStopsKeys"
+          :key="stop"
+          @click="selectBusStop(stop)"
+          :class="{ content: true, selected: selectedBusLineStop === stop }"
+        >
+          {{ stop }}
+        </div>
+      </div>
+    </div>
+
+    <span class="selection-area-empty" v-if="!sortedBusLineStops">
+      Please select the bus line first
+    </span>
+    <div v-else class="selection-area">
+      <div class="bus-lines-container">
+        <div class="title">Bus Stop: Rondo Czyżyńskie 10</div>
+        <div class="subtitle">Time</div>
+        <div
+          v-for="(stop, index) in sortedBusLineStops"
+          :key="index"
+          class="content"
+        >
+          {{ stop.time }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+const busLineStopsKeys = computed(() => store.getters.getBusLineStopsKeys);
+const selectedBusLineStop = computed(
+  () => store.getters.getSelectedBusLineStop
+);
+const sortedBusLineStops = computed(() => store.getters.getSortedBusLineStops);
+
+const selectBusStop = (stop) => {
+  store.dispatch("selectBusStop", stop);
+};
+</script>
+
+<style lang="scss" scoped>
+.selection-container {
+  display: flex;
+  gap: 8px;
+  justify-content: space-between;
+
+  .selection-area-empty {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50%;
+    border-radius: 4px;
+    border: 2px dashed #9a9da4;
+    background: #fff;
+    color: #63666e;
+    text-align: center;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+    min-height: 200px;
+  }
+  .selection-area {
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    border-radius: 4px;
+    border: 2px white;
+    background: #fff;
+    color: #33373c;
+    text-align: center;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+    min-height: 100px;
+
+    .bus-lines-container {
+      text-align: left;
+      width: 100%;
+      height: 400px;
+      overflow-y: auto;
+
+      .title {
+        display: flex;
+        align-items: center;
+        height: 56px;
+        color: #1a1a1a;
+        font-size: 14px;
+        font-weight: 600;
+        line-height: 24px;
+        padding: 0 24px;
+      }
+      .subtitle {
+        display: flex;
+        align-items: center;
+        height: 56px;
+        color: #33373c;
+        font-size: 12px;
+        font-weight: 600;
+        line-height: 16px;
+        padding: 0 24px;
+      }
+      .content {
+        display: flex;
+        align-items: center;
+        height: 56px;
+        color: #33373c;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 16px;
+        padding: 0 24px;
+        cursor: pointer;
+        border-top: 1px solid #ccc;
+
+        &.selected {
+          background-color: #f8f8fb;
+          color: #1952e1;
+        }
+
+        &:hover {
+          background-color: #f8f8fb;
+        }
+      }
+
+      /* Hide the scrollbar for Chrome, Safari, and Opera */
+      &::-webkit-scrollbar {
+        display: none;
+      }
+      -ms-overflow-style: none; /* IE and Edge */
+      scrollbar-width: none; /* Firefox */
+
+      .selected {
+      }
+    }
+  }
+}
+</style>
