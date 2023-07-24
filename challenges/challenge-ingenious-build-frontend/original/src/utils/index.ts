872a78e67b989c20d1386d3d1bca95336bc05fc2
Vinicius Souza <viniciuspzt@gmail.com>
2023-07-24 02:31:27 +0100

    create the base behavior for store, bus lines, bus stops, and time

---
+++ challenges/challenge-ingenious-build-frontend/original/src/utils/index.ts
@@ -0,0 +1,32 @@
+import { Stop, GroupedStops } from "@/interfaces";
+
+export function groupStopsByLine(stops: Stop[]) {
+  const groupedStops: { [key: number]: Stop[] } = stops.reduce(
+    (result: { [key: number]: Stop[] }, stop) => {
+      const { line } = stop;
+      if (!result[line]) {
+        result[line] = [];
+      }
+      result[line].push(stop);
+      return result;
+    },
+    {}
+  );
+
+  return groupedStops;
+}
+
+export function groupStopsByStopName(stops: Stop[]): GroupedStops {
+  return stops.reduce((groupedStops: GroupedStops, stop: Stop) => {
+    const { stop: stopName } = stop;
+    groupedStops[stopName] = [...(groupedStops[stopName] || []), stop];
+    return groupedStops;
+  }, {});
+}
+
+export function parseTime(timeStr: string): Date {
+  const [hours, minutes] = timeStr.split(":").map(Number);
+  const dateObj = new Date();
+  dateObj.setHours(hours, minutes, 0, 0);
+  return dateObj;
+}
