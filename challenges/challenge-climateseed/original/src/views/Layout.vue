3444222dae51bfd0afcc85944a276f91810fcd97
Vinicius Souza <viniciuspzt@gmail.com>
2024-03-20 11:33:05 +0000

    add second graph type bar

--- challenges/challenge-climateseed/original/src/views/Layout.vue
+++ challenges/challenge-climateseed/original/src/views/Layout.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
 import { useDataStore } from '../stores/data';
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 
 const dataStore = useDataStore();
-const { aggregatedData, chartOptions } = storeToRefs(dataStore);
-const { series, legend } = aggregatedData.value
+const { aggregatedData, aggregatedDataTypeBar, chartOptions } = storeToRefs(dataStore);
+let { series } = aggregatedData.value;
+
+const isDonutChart = ref(true);
+
+const toggleChartType = () => {
+  isDonutChart.value = !isDonutChart.value;
+};
 </script>
 
 <template>
   <div>
-    <apexchart :series="series" :options="chartOptions" type="donut" />
-    {{ legend }}
-    {{ chartOptions }}
+    <button @click="toggleChartType">Toggle Chart Type</button>
+    <div v-if="isDonutChart">
+      <apexchart :series="series" :options="chartOptions" type="donut" />
+    </div>
+    <div v-else>
+      <apexchart :series="aggregatedDataTypeBar" :options="chartOptions" type="bar" />
+    </div>
   </div>
 </template>
