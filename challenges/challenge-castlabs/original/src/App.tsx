4b443e24a00b66dc881a469f6898aa887f6f57f1
Vinicius Souza <viniciuspzt@gmail.com>
2024-06-10 20:57:42 +0100

    get started apollographql docs

--- challenges/challenge-castlabs/original/src/App.tsx
+++ challenges/challenge-castlabs/original/src/App.tsx
@@ -1,13 +1,45 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { useQuery, gql } from "@apollo/client";
 import './App.css'
+
+
+const GET_LOCATIONS = gql`
+  query getLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <DisplayLocations />
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
