05a126f55cdf89004d9ffc031f961eb102581f29
Vinicius Souza <viniciuspzt@gmail.com>
2024-06-11 13:00:26 +0100

    add react router, create episodes page

--- challenges/challenge-castlabs/original/src/App.tsx
+++ challenges/challenge-castlabs/original/src/App.tsx
@@ -3,14 +3,13 @@ import { useQuery } from '@apollo/client';
 import { LIST_EPISODES } from './graphql/queries'
 import { Episode, Episodes, SearchEpisode } from './interfaces';
 import debounce from 'lodash.debounce';
+import { Link } from 'react-router-dom';
 
 function App() {
   const [search, setSearch] = useState('');
   const { data, loading, error, refetch } = useQuery<Episodes, SearchEpisode>(LIST_EPISODES, {
     variables: { search },
   });
-
-  console.log(data)
 
   const debouncedSearch = useCallback(
     debounce((value) => {
@@ -38,9 +37,9 @@ function App() {
       <ul>
         {data?.listEpisodes?.map((episode: Episode) => (
           <li key={episode.id} className="border-b p-2">
-            <p>
+            <Link to={`/episode/${episode.id}`}>
               {episode.title} (Season {episode.seasonNumber}, Episode {episode.episodeNumber})
-            </p>
+            </Link>
           </li>
         ))}
       </ul>
