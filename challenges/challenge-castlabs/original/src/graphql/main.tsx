bb7815a2e90cd21fdc76f53c180a8a980e8fffdd
Vinicius Souza <viniciuspzt@gmail.com>
2024-06-11 13:10:09 +0100

    add prettier, move GET_EPISODE_BY_ID gql query

--- challenges/challenge-castlabs/original/src/graphql/main.tsx
+++ challenges/challenge-castlabs/original/src/graphql/main.tsx
@@ -1,35 +1,37 @@
-import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-import { createClient } from 'graphql-ws';
-import { getMainDefinition } from '@apollo/client/utilities';
+import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 const httpLink = new HttpLink({
   uri: import.meta.env.VITE_GRAPHQL_API_URL,
   headers: {
-    'x-api-key': import.meta.env.VITE_GRAPHQL_API_KEY,
+    "x-api-key": import.meta.env.VITE_GRAPHQL_API_KEY,
   },
 });
 
-const wsLink = new GraphQLWsLink(createClient({
-  url: import.meta.env.VITE_GRAPHQL_API_URL,
-  connectionParams: {
-    headers: {
-      'x-api-key': import.meta.env.VITE_GRAPHQL_API_KEY,
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: import.meta.env.VITE_GRAPHQL_API_URL,
+    connectionParams: {
+      headers: {
+        "x-api-key": import.meta.env.VITE_GRAPHQL_API_KEY,
+      },
     },
-  },
-}));
+  }),
+);
 
 const splitLink = split(
   ({ query }) => {
     const definition = getMainDefinition(query);
-    console.log(definition)
+    console.log(definition);
     return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
     );
   },
   wsLink,
-  httpLink
+  httpLink,
 );
 
 const client = new ApolloClient({
--- challenges/challenge-castlabs/original/src/interfaces/index.ts
+++ challenges/challenge-castlabs/original/src/interfaces/index.ts
@@ -1,18 +1,18 @@
 export interface Episode {
-    id: string;
-    series: string;
-    title: string;
-    description: string;
-    seasonNumber: number;
-    episodeNumber: number;
-    releaseDate: string;
-    imdbId: string;
+  id: string;
+  series: string;
+  title: string;
+  description: string;
+  seasonNumber: number;
+  episodeNumber: number;
+  releaseDate: string;
+  imdbId: string;
 }
 
 export interface Episodes {
-    listEpisodes: Episode[];
+  listEpisodes: Episode[];
 }
 
 export interface SearchEpisode {
-    search: string;
+  search: string;
 }
