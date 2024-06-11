ec455cf7af4feb331f7a420b2d12479bf54788f9
Vinicius Souza <viniciuspzt@gmail.com>
2024-06-11 22:53:00 +0100

    create loading and error components

--- challenges/challenge-castlabs/original/src/pages/Episode.tsx
+++ challenges/challenge-castlabs/original/src/pages/Episode.tsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_EPISODE_BY_ID } from "@graphql/queries";
-import Header from "@components/Header";
 import { useDebouncedEpisodesSearch } from "@/hooks/useDebouncedEpisodesSearch";
+import Header from "@components/Header";
+import Loading from "@components/Loading";
+import Error from "@components/Error";
 
 export default function Episode() {
   const { search, handleSearchChange } = useDebouncedEpisodesSearch();
@@ -26,27 +28,19 @@ export default function Episode() {
       <Header search={search} onSearchChange={handleSearchChange} />
 
       <div className="container mx-auto mt-16 p-4">
+        {loading && <Loading />}
+        {error && <Error />}
         <div className="rounded bg-white p-6 shadow-md">
           <h1 className="mb-4 text-3xl font-bold">{episode.title}</h1>
           <p className="mb-4 text-gray-700">{episode.description}</p>
-          <div className="mb-4">
-            <span className="font-semibold">Series:</span> {episode.series}
-          </div>
-          <div className="mb-4">
-            <span className="font-semibold">Season:</span>{" "}
-            {episode.seasonNumber}
-          </div>
-          <div className="mb-4">
-            <span className="font-semibold">Episode:</span>{" "}
-            {episode.episodeNumber}
-          </div>
-          <div className="mb-4">
-            <span className="font-semibold">Release Date:</span>{" "}
-            {episode.releaseDate}
-          </div>
-          <div className="mb-4">
-            <span className="font-semibold">IMDB ID:</span> {episode.imdbId}
-          </div>
+          <p className="mb-4 font-semibold">Series:</p> {episode.series}
+          <p className="mb-4 font-semibold">Season:</p>
+          {episode.seasonNumber}
+          <p className="mb-4 font-semibold">Episode:</p>
+          {episode.episodeNumber}
+          <p className="mb-4 font-semibold">Release Date:</p>
+          {episode.releaseDate}
+          <p className="mb-4 font-semibold">IMDB ID:</p> {episode.imdbId}
         </div>
       </div>
     </div>
