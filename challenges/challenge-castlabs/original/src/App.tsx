c9859a4a2867fd41be9cc40f0f1acfd585df8cfa
Vinicius Souza <viniciuspzt@gmail.com>
2024-06-11 13:25:22 +0100

    update tailwind config, add prettier-plugin-tailwindcss

--- challenges/challenge-castlabs/original/src/App.tsx
+++ challenges/challenge-castlabs/original/src/App.tsx
@@ -4,6 +4,7 @@ import { LIST_EPISODES } from "./graphql/queries";
 import { Episode, Episodes, SearchEpisode } from "./interfaces";
 import debounce from "lodash.debounce";
 import { Link } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   const [search, setSearch] = useState("");
@@ -27,27 +28,23 @@ function App() {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <input
-        type="text"
-        placeholder="Search episodes..."
-        className="border p-2 w-full mb-4"
-        value={search}
-        onChange={handleSearchChange}
-      />
-      {loading && <p>Loading...</p>}
-      {error && <p>Error loading episodes.</p>}
-      <ul>
-        {data?.listEpisodes?.map((episode: Episode) => (
-          <li key={episode.id} className="border-b p-2">
-            <Link to={`/episode/${episode.id}`}>
-              {episode.title} (Season {episode.seasonNumber}, Episode{" "}
-              {episode.episodeNumber})
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Header search={search} onSearchChange={handleSearchChange} />
+      <div className="container mx-auto p-4">
+        {loading && <p>Loading...</p>}
+        {error && <p>Error loading episodes.</p>}
+        <ul>
+          {data?.listEpisodes?.map((episode: Episode) => (
+            <li key={episode.id} className="border-b p-2">
+              <Link to={`/episode/${episode.id}`}>
+                {episode.title} (Season {episode.seasonNumber}, Episode{" "}
+                {episode.episodeNumber})
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
