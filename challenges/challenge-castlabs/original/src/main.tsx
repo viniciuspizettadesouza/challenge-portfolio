1ceb403871c3f732229ee2f7ef2aa64ce5850938
Vinicius Souza <viniciuspzt@gmail.com>
2024-06-11 13:00:26 +0100

    add react router, create episodes page

--- challenges/challenge-castlabs/original/src/main.tsx
+++ challenges/challenge-castlabs/original/src/main.tsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import client from './graphql/main.tsx';
 import App from './App.tsx'
+import Episode from './pages/Episode.tsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/episode/:id",
+    element: <Episode />,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
   </ApolloProvider>
 )
