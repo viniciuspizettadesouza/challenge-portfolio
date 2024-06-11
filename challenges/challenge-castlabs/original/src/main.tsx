a034b0bfff4b5864da0ab849121e683ed7a952dc
Vinicius Souza <viniciuspzt@gmail.com>
2024-06-11 14:25:53 +0100

    move router provider to App

--- challenges/challenge-castlabs/original/src/main.tsx
+++ challenges/challenge-castlabs/original/src/main.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
 import { ApolloProvider } from "@apollo/client";
 import client from "@graphql/main.tsx";
 import "./index.css";
-import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ApolloProvider client={client}>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <App />
     </React.StrictMode>
   </ApolloProvider>,
 );
