fc284d1daae5fa85e519e5309e253cfa590d6046
Vinicius Souza <viniciuspzt@gmail.com>
2026-07-26 19:03:58 +0100

    feat(portfolio): integrate React and Vue pilot demos

---
+++ challenges/challenge-salsify/demo/src/index.tsx
@@ -0,0 +1,11 @@
+import OriginalApp from "../../original/src/App";
+import "./styles.css";
+
+export default function SalsifyDemo() {
+  return (
+    <div className="salsify-demo">
+      <OriginalApp />
+    </div>
+  );
+}
+
---
+++ challenges/challenge-vue/demo/src/index.ts
@@ -0,0 +1,5 @@
+import OriginalApp from "../../original/src/App.vue";
+import "./styles.css";
+
+export default OriginalApp;
+
